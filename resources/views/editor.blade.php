24d916f78ac503fc26fcb0d86128fcbb49bd5215
riasvdv <rias@spatie.be>
2022-11-09 10:51:59 +0000

    Update version constraint

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -1,67 +1,220 @@
-@push('endHead')
-    <link rel="stylesheet" href="https://uicdn.toast.com/editor/3.1.5/toastui-editor.min.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/themes/prism.min.css" />
-    <link rel="stylesheet" href="https://uicdn.toast.com/editor-plugin-code-syntax-highlight/3.0.0/toastui-editor-plugin-code-syntax-highlight.min.css" />
-    <link rel="stylesheet" href="https://uicdn.toast.com/editor-plugin-table-merged-cell/3.0.1/toastui-editor-plugin-table-merged-cell.min.css" />
-    <link rel="stylesheet" href="https://uicdn.toast.com/tui-color-picker/latest/tui-color-picker.min.css" />
-    <link rel="stylesheet" href="https://uicdn.toast.com/editor-plugin-color-syntax/latest/toastui-editor-plugin-color-syntax.min.css" />
-@endpush
-@push('modals')
-    <script src="https://uicdn.toast.com/chart/latest/toastui-chart.min.js"></script>
-    <script src="https://uicdn.toast.com/tui-color-picker/latest/tui-color-picker.min.js"></script>
-    <script src="https://uicdn.toast.com/editor/3.1.5/toastui-editor-all.min.js"></script>
-    <script src="https://uicdn.toast.com/editor-plugin-code-syntax-highlight/3.0.0/toastui-editor-plugin-code-syntax-highlight-all.min.js"></script>
-    <script src="https://uicdn.toast.com/editor-plugin-table-merged-cell/3.0.1/toastui-editor-plugin-table-merged-cell.min.js"></script>
-    <script src="https://uicdn.toast.com/editor-plugin-color-syntax/3.0.3/toastui-editor-plugin-color-syntax.min.js"></script>
-    @php
-        try {
-            $manifest = json_decode(file_get_contents(public_path('vendor/mailcoach-markdown-editor/manifest.json')), true);
-        } catch (Exception $e) {
-            $manifest = null;
+<div class="form-grid">
+    <style>
+        .cm-s-easymde .cm-header-1 {
+            font-size: 1.875rem
         }
-    @endphp
-    @if ($manifest)
-        <script type="module" src="/vendor/mailcoach-markdown-editor/{{ $manifest['resources/js/editor.js']['file'] }}"></script>
-    @else
-        <script type="module" src="http://localhost:3000/@vite/client"></script>
-        <script type="module" src="http://localhost:3000/resources/js/editor.js"></script>
+
+        .cm-s-easymde .cm-header-2 {
+            font-size: 1.5rem
+        }
+
+        .cm-s-easymde .cm-header-3 {
+            font-size: 1.25rem
+        }
+
+        .cm-s-easymde .cm-header-4 {
+            font-size: 1.125rem
+        }
+
+        .cm-s-easymde .cm-header-5 {
+            font-size:1.125rem
+        }
+
+        .cm-s-easymde .cm-header-6 {
+            font-size:1rem
+        }
+
+        .cm-s-easymde .cm-comment {
+            background: none;
+        }
+
+        .cm-keyword {color: #708;}
+        .cm-atom {color: #219;}
+        .cm-number {color: #164;}
+        .cm-def {color: #00f;}
+        .cm-variable,
+        .cm-punctuation,
+        .cm-property,
+        .cm-operator {}
+        .cm-variable-2 {color: #05a;}
+        .cm-formatting-list, .cm-formatting-list + .cm-variable-2 {color: #000;}
+        .cm-variable-3, .cm-s-default .cm-type {color: #085;}
+        .cm-comment {color: #a50;}
+        .cm-string {color: #a11;}
+        .cm-string-2 {color: #f50;}
+        .cm-meta {color: #555;}
+        .cm-qualifier {color: #555;}
+        .cm-builtin {color: #30a;}
+        .cm-bracket {color: #997;}
+        .cm-tag {color: #170;}
+        .cm-attribute {color: #00c;}
+        .cm-hr {color: #999;}
+        .cm-link {color: #00c;}
+    </style>
+    <script>
+        function debounce(func, timeout = 300){
+            let timer;
+            return (...args) => {
+                clearTimeout(timer);
+                timer = setTimeout(() => { func.apply(this, args); }, timeout);
+            };
+        }
+
+        window.init = function() {
+            let editor = new EasyMDE({
+                autoDownloadFontAwesome: false,
+                element: this.$refs.editor,
+                uploadImage: true,
+                placeholder: '{{ __mc('Start writing…') }}',
+                initialValue: this.markdown,
+                spellChecker: false,
+                autoSave: false,
+                status: [{
+                            className: "upload-image",
+                            defaultValue: ''
+                        }],
+                toolbar: [
+                    "heading", "bold", "italic", "link",
+                    "|",
+                    "quote", "unordered-list", "ordered-list", "table",
+                    "|",
+                    {
+                        name: "upload-image",
+                        action: EasyMDE.drawUploadedImage,
+                        className: "fa fa-image",
+                    },
+                    "undo",
+                    { // When FontAwesome is not auto downloaded, this loads the correct icon
+                        name: "redo",
+                        action: EasyMDE.redo,
+                        className: "fa fa-redo",
+                        title: "Redo",
+                    },
+                ],
+                imageAccept: 'image/png, image/jpeg, image/gif, image/avif',
+                imageUploadFunction: function(file, onSuccess, onError) {
+                    if (file.size > 1024 * 1024 * 2) {
+                        return onError('File cannot be larger than 2MB.');
+                    }
+
+                    if (file.type.split('/')[0] !== 'image') {
+                        return onError('File must be an image.');
+                    }
+
+                    const data = new FormData();
+                    data.append('file', file);
+
+                    fetch('{{ action(\Spatie\Mailcoach\Http\Api\Controllers\UploadsController::class) }}', {
+                        method: 'POST',
+                        body: data,
+                        credentials: 'same-origin',
+                        headers: {
+                            'X-CSRF-Token': '{{ csrf_token() }}',
+                        },
+                    })
+                        .then(response => response.json())
+                        .then(({ success, file }) => {
+                            if (! success) {
+                                return onError();
+                            }
+
+                            onSuccess(file.url);
+                        });
+                },
+            });
+
+            editor.codemirror.on("change", debounce(() => {
+                this.markdown = editor.value();
+                this.$refs.editor.dirty = true;
+                renderToHtml(this);
+            }));
+
+            this.$watch('theme', () => {
+                this.markdown = editor.value();
+                renderToHtml(this);
+            });
+
+            function renderToHtml(instance) {
+                fetch('{{ route('mailcoach-markdown-editor.render-markdown') }}', {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        markdown: instance.markdown,
+                        theme: instance.theme,
+                    })
+                })
+                    .then(response => response.text())
+                    .then(html => instance.html = html);
+            }
+        }
+    </script>
+    @if ($model->hasTemplates())
+        <x-mailcoach::template-chooser :clearable="false" />
     @endif
-@endpush
-<div class="border rounded-md">
-    <div class="bg-white min-h-full rounded-md">
-        <div id="markdown-editor"
-             data-options="{{ json_encode(config('mailcoach-markdown-editor.options')) }}"
-             data-structured-html="{{ $body ?? '' }}"
-             data-upload="{{ action([\Spatie\MailcoachMarkdownEditor\Http\Controllers\EditorController::class, 'upload']) }}"
-        ></div>
-        @error('html')
-            <p class="form-error" role="alert">{{ $message }}</p>
-        @enderror
+
+    @foreach($template?->fields() ?? [['name' => 'html', 'type' => 'editor']] as $field)
+        <x-mailcoach::editor-fields :name="$field['name']" :type="$field['type']" :label="$field['name'] === 'html' ? 'Markdown' : null">
+            <x-slot name="editor">
+                <div class="markup markup-editor markup-lists markup-links markup-code"
+                    wire:ignore x-data="{
+                    html: @entangle('templateFieldValues.' . $field['name'] . '.html'),
+                    markdown: @entangle('templateFieldValues.' . $field['name'] . '.markdown'),
+                    theme: @entangle('templateFieldValues.' . $field['name'] . '.theme'),
+                    init: init,
+                }">
+                    <textarea x-ref="editor" data-dirty-check></textarea>
+
+                    <div class="form-field -mt-4 mb-4" x-show="markdown.includes('```')" x-cloak>
+                        <label class="label" for="theme">
+                            Syntax highlighting theme
+                        </label>
+                        <div class="select">
+                            <select class="" name="theme" id="theme" x-model="theme">
+                                <option>dark-plus</option>
+                                <option>dracula-soft</option>
+                                <option>dracula</option>
+                                <option>github-dark-dimmed</option>
+                                <option>github-dark</option>
+                                <option>github-light</option>
+                                <option>hc_light</option>
+                                <option>light-plus</option>
+                                <option>material-darker</option>
+                                <option>material-default</option>
+                                <option>material-lighter</option>
+                                <option>material-ocean</option>
+                                <option>material-palenight</option>
+                                <option>min-dark</option>
+                                <option>min-light</option>
+                                <option>monokai</option>
+                                <option>nord</option>
+                                <option>one-dark-pro</option>
+                                <option>poimandres</option>
+                                <option>rose-pine-dawn</option>
+                                <option>rose-pine-moon</option>
+                                <option>rose-pine</option>
+                                <option>slack-dark</option>
+                                <option>slack-ochin</option>
+                                <option>solarized-dark</option>
+                                <option>solarized-light</option>
+                                <option>vitesse-dark</option>
+                                <option>vitesse-light</option>
+                            </select>
+                            <div class="select-arrow">
+                                <i class="fas fa-angle-down"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </x-slot>
+        </x-mailcoach::editor-fields>
+    @endforeach
+
+    <div class="-mt-4 flex gap-4">
+        <x-mailcoach::replacer-help-texts :model="$model" />
+        <a class="link-dimmed" href="https://www.markdownguide.org/basic-syntax/" target="_blank">Markup syntax</a>
     </div>
+    <x-mailcoach::editor-buttons :preview-html="$fullHtml" :model="$model" />
 </div>
-
-<input type="hidden" id="body" name="structured_html[body]" value="{{ old('structured_html.body', $body ?? '') }}">
-<input type="hidden" id="template" name="structured_html[template]" value="{{ old('structured_html.template', $template ?? '') }}">
-<input type="hidden" id="html" name="html" value="{{ old('html', $html) }}" data-html-preview-source>
-<div class="form-buttons">
-    <x-mailcoach::button id="save" :label="__('Save content')"/>
-    <x-mailcoach::button-secondary data-modal-trigger="edit-template" :label="__('Edit template')"/>
-    <x-mailcoach::button-secondary id="preview" :label="__('Preview')"/>
-    @if ($showTestButton)
-        <x-mailcoach::button-secondary data-modal-trigger="send-test" :label="__('Send Test')"/>
-    @endif
-</div>
-
-@push('modals')
-    <x-mailcoach::modal :title="__('Edit template')" name="edit-template" large>
-        <div class="p-6">
-            <p class="mb-6">{!! __('Make sure to include a <code>::content::</code> placeholder where the Editor‘s content should go.') !!}</p>
-
-            <x-mailcoach::html-field name="structured_html[template]" :value="old('structured_html.template', $template ?? '')" />
-
-            <div class="form-buttons">
-                <x-mailcoach::button data-modal-confirm="edit-template" type="button" :label=" __('Save')" />
-            </div>
-        </div>
-    </x-mailcoach::modal>
-@endpush
